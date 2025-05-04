4fc79a9f71218760188cfadd80b5a5b9cea20f07
maximgoranov <119817217+maximgoranov@users.noreply.github.com>
2025-05-04 00:32:34 -0400

    SteamIntegration (50%)
    Functions made for getting gameData based on steamID, and for converting appID to game name for storage in database.
    
    Still to do is a way to iterate through all games and put the game name, and then store in the database per user.
    
    Possible issues: There is still no logic for if a user is using a private profile.

--- python Server/models.py
+++ python Server/models.py
@@ -1,7 +1,7 @@
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID
 from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTableUUID
 from database import Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, DateTime, Text, Integer, Boolean
 
 
 #  Class user, which inherits SQLAlchemyBaseUserTableUUID (data columns like id, email, hashed password, etc)
@@ -16,3 +16,10 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
 class AccessToken(SQLAlchemyBaseAccessTokenTableUUID, Base):
     pass
 
+class SteamData(Base):
+    __tablename__ = "steamData"
+    entry_id = Column(Integer, primary_key=True, autoincrement=True)  #not explicitly necessary, this is mainly a debugging feature. If you have an issue, use this to delete....
+    steam_id = Column(String(length=64), index=True)
+    date = Column(DateTime)
+    payload = Column(Text)
+    active = Column(Boolean, default=True)
--- python Server/steamintegration.py
+++ python Server/steamintegration.py
@@ -1,8 +1,9 @@
 #       Add user fields for SteamID and Username
-# TODO:
 #       Add separate model with relational mapping to user's steamID
 #       Add fields for owned games, playtime, last played, ...
+#                 Create column in database for activating auto update from steam
 #       Make functions to make API calls to steam, receive JSON payload
+# TODO:
 #       Parse JSON payload, fill fields based on userID
 
 #                                  primary   required        nullable               nullable
@@ -19,4 +20,46 @@ Placeholder:
 async def get_steamData_db(session: AsyncSession = Depends(get_async_session)):
     yield ----------(session, steamData)
 """
+import asyncio
+import httpx
+#from datetime import datetime
+#from models import SteamData
+#from authentication import settings
+class settings:
+    STEAM_API_KEY="AE88E413FD5384D1164CC3599BA1A82C"
 
+async def steamRequest(steamID):
+    base_url = "https://api.steampowered.com"
+    endpoint = f"{base_url}/IPlayerService/GetOwnedGames/v0001"
+    params = {
+        "key": settings.STEAM_API_KEY,
+        "steamid": steamID
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(endpoint, params=params)
+        response.raise_for_status()
+        return response.json()
+
+async def steamGetAppName(appID):
+    base_url = "https://store.steampowered.com"
+    endpoint = f"{base_url}/api/appdetails"
+    params = {
+        "appids":appID
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(endpoint, params=params)
+        response.raise_for_status()
+        return response.json()[str(appID)]["data"]["name"]
+
+
+
+
+async def test():
+    """
+    result = await steamRequest("76561198259864303")
+
+    print(result)
+    """
+    appName = await steamGetAppName(420)
+    print(appName)
+#asyncio.run(test())
